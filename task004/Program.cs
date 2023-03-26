12dfb2f10a5b49bbc8cf9a712803a5d5c3c7c605
Sylm@n <l.musilmanbekova12@gmail.com>
2023-03-26 20:04:00 +0300

    added second task with finding max of 3 numbers

---
+++ task004/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a = 0;
+int b = 0;
+int c = 0;
+
+Console.WriteLine("наберите три числа (через Enter) и после снова нажмите на Enter");
+
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b) && int.TryParse(Console.ReadLine(), out c))
+{
+
+  if (a > b && a > c)
+  {
+    Console.Write(a);
+    Console.WriteLine(" больше ");
+  }
+  else if (b > a && b > c)
+  {
+    Console.Write(b);
+    Console.WriteLine(" больше ");
+  }
+  else if (c > a && c > b)
+  {
+    Console.Write(c);
+    Console.WriteLine(" больше ");
+  }
+}
