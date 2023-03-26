0cb4565cf463604a36d4a6df831366911e64e3e9
Sylm@n <l.musilmanbekova12@gmail.com>
2023-03-26 19:43:31 +0300

    added first task with finding max and min of numbers

---
+++ task002/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 0;
+int b = 0;
+
+Console.WriteLine("наберите два числа (через Enter) и после снова нажмите на Enter");
+
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+{
+
+  if (a > b)
+  {
+    Console.Write(a);
+    Console.WriteLine(" больше ");
+    Console.Write(b);
+    Console.Write(" меньше ");
+  }
+  else
+  {
+    Console.Write(b);
+    Console.WriteLine(" больше ");
+    Console.Write(a);
+    Console.Write(" меньше ");
+  }
+}
