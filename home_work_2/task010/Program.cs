7dd5a5f7373b81f29f49f3d5ebb389a56eb9c00e
Sylm@n <l.musilmanbekova12@gmail.com>
2023-04-12 17:34:35 +0300

    modified task010

--- home_work_2/task010/Program.cs
+++ home_work_2/task010/Program.cs
@@ -29,20 +29,3 @@ else
   System.Console.WriteLine("Надо было очень постараться чтобы вылезла эта ошибка. Попробуйте все же ввести цифры, для корректной работы программы.");
 }
 
-
-/*Console.WriteLine("Введите трехзначное число");
-int.TryParse(Console.ReadLine()!, out int k);
-
-if (k > 999 || k < 100)
-{
-  Console.WriteLine("Не трехзначное число!!!");
-  return;
-}
-int GetSecond(int k)
-{
-  while (k >= 100) k /= 10;
-  int d = k % 10;
-  return d;
-}
-Console.WriteLine(GetSecond(k));
-*/
