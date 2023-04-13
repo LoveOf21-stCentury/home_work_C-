3c7d79e46ff623e8ec8ac689c12e69366dfcc1ac
Sylm@n <l.musilmanbekova12@gmail.com>
2023-04-13 20:27:51 +0300

    doned task15

--- home_work_2/task015/Program.cs
+++ home_work_2/task015/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void WeekEnd()
+{
+  bool isWorking = true;
+
+  while (isWorking)
+  {
+    Console.WriteLine("Введите цифру дня недели (Для выхода напишите 'end')"); //эта строка будет выводиться снова после вывода результата, чтобы была возможность не перезапускать код, а продолжить проверку на "рабочий день"
+
+    string day = Console.ReadLine()!;
+
+    switch (day)
+    {
+      case "end":
+        isWorking = false;
+        break;
+      case "1":
+        System.Console.WriteLine("понедельник - нет");
+        break;
+      case "2":
+        System.Console.WriteLine("вторник - нет");
+        break;
+      case "3":
+        System.Console.WriteLine("среда - нет");
+        break;
+      case "4":
+        System.Console.WriteLine("четверг - нет");
+        break;
+      case "5":
+        System.Console.WriteLine("пятница - нет");
+        break;
+      case "6":
+        System.Console.WriteLine("суббота - да");
+        break;
+      case "7":
+        System.Console.WriteLine("воскресенье - да");
+        break;
+      default:
+        System.Console.WriteLine("ошибка, введите цифру от 1 до 7");
+        break;
+    }
+  }
+}
+
+WeekEnd();
