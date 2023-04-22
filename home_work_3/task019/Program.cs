9bc6467b5bdae3bc4083cfaa304125e941510b03
Sylman <l.musilmanbekova12@gmail.com>
2023-04-22 02:51:06 -0700

    completed task019

--- home_work_3/task019/Program.cs
+++ home_work_3/task019/Program.cs
@@ -3,3 +3,23 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+Console.WriteLine("Write number of 5 numbers");
+int a = int.Parse(Console.ReadLine());
+if (a > 9999 && a < 100000)
+{
+    char[] arr = a.ToString().ToCharArray();
+    if (arr[0] == arr[4] && arr[1] == arr[3])
+    {
+        System.Console.WriteLine("Polindrome!");
+    }
+    else
+    {
+        System.Console.WriteLine("Not polindrome!");
+    }
+}
+else
+{
+    System.Console.WriteLine("No. Need number of 5 numbers");
+}
+
+
