05869fc2ec9eb629393f2f79ffe6afec5eec8a6f
Sylman <l.musilmanbekova12@gmail.com>
2023-04-22 11:55:49 -0700

    completed task025

--- home_work_4/task025/Program.cs
+++ home_work_4/task025/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.WriteLine("write your num a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("write your num b: ");
+double b = double.Parse(Console.ReadLine());
+
+double Change(double a, double b)
+{
+    double r = Math.Pow(a, b);
+    return r;
+}
+
+Console.WriteLine(Change(a, b));
+
