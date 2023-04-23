afee93f10510a2e26fa6311a7127ea779b55cb74
Sylman <l.musilmanbekova12@gmail.com>
2023-04-23 02:40:24 -0700

    completed task027

---
+++ home_work_4/task027/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+System.Console.WriteLine("your number: ");
+int x = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (x > 0)
+{
+
+    sum = sum + x % 10;
+    x = x / 10;
+
+}
+Console.WriteLine(sum);
+
+
