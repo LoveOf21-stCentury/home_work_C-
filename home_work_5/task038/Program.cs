99bc517f9ebc1c506d4e83aaf8eb824d24d09efc
Sylman <l.musilmanbekova12@gmail.com>
2023-04-29 09:23:51 -0700

    completed task038

--- home_work_5/task038/Program.cs
+++ home_work_5/task038/Program.cs
@@ -1,58 +1,88 @@
-﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+﻿/* 
+using System;
+using System.Security.Cryptography;
+string input(string text)
+{
+    Console.Write($"{text}");
+    return Console.ReadLine();
+} */
+
+/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
 [3,21 7,04 22,93 -2,71 78,24] -> 80,95 */
 
 void FillArray(double[] numbers)
 {
     Random rnd = new Random();
-    System.Console.WriteLine("array is: ");
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = rnd.NextDouble(-9, 10);
+        numbers[i] = Math.Round(rnd.NextDouble() * 10 - 5, 1);
     }
-    /* for (int i = 0; i < nums.Length; i++)
-    {
-        System.Console.WriteLine($"write {i + 1} element: ");
-        nums[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    System.Console.WriteLine(); */
 }
 
 void PrintArray(double[] numbers)
 {
     for (int i = 0; i < numbers.Length; i++)
     {
-        System.Console.Write(numbers[i] + " ");
+        System.Console.Write($"{numbers[i]}   ");
     }
     System.Console.WriteLine();
 }
 
-double SearchDif(double[] numbers)
+double FindingMax(double[] numbers)
 {
     double max = numbers[0];
-    double min = numbers[0];
     for (int i = 0; i < numbers.Length; i++)
     {
+
         if (numbers[i] > max)
         {
             max = numbers[i];
         }
-        else if (numbers[i] < min)
+    }
+    return max;
+}
+
+double FindingMin(double[] numbers)
+{
+    double min = numbers[0];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < min)
         {
             min = numbers[i];
         }
     }
-    return numbers.Max() - numbers.Min();
+    return min;
 }
 
 void Task38()
 {
     System.Console.WriteLine("How long your array will be?");
-    double size = Convert.ToDouble(Console.RedLine);
-    
-    int[] numbers = new double[size];
+    int size = int.Parse(Console.ReadLine());
+    double[] numbers = new double[size];
     FillArray(numbers);
     PrintArray(numbers);
-    //System.Console.WriteLine($"difference btw nums is: {SearchDif(numbers)}");
+    double max = FindingMax(numbers);
+    double min = FindingMin(numbers);
+    double result = max - min;
+    System.Console.WriteLine($"difference btw nums is: {result}");
 }
 
 Task38();
+
+
+/* double min = numbers[0];
+  for (int i = 0; i < numbers.Length; i++)
+  {
+      if (numbers[i] > max)
+      {
+          max = numbers[i];
+          System.Console.WriteLine($"Max is {max}");
+      }
+      else if (numbers[i] < min)
+      {
+          min = numbers[i];
+          System.Console.WriteLine($"Min is {min}");
+      }
+  }*/
