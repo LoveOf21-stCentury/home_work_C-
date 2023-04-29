492e08fa9d07b69ad7bc1416389bce0b60e9bc0c
Sylman <l.musilmanbekova12@gmail.com>
2023-04-29 03:55:30 -0700

    added folder for home_work_5 and completed task36

---
+++ home_work_5/task036/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов с нечётными индексами.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+void FillArry(int[] numbers)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rnd.Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        System.Console.Write(nums[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int GetPositiveSum(int[] numbers)
+{
+    int sum = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) sum += numbers[i];
+    }
+    return sum;
+}
+
+void Task36()
+{
+    System.Console.WriteLine("How long your array will be?");
+    int size = int.Parse(Console.ReadLine());
+    int[] numbers = new int[size];
+    FillArry(numbers);
+    PrintArray(numbers);
+    int sum = GetPositiveSum(numbers);
+    System.Console.WriteLine($"sum of positive elements {sum}");
+}
+
+Task36();
