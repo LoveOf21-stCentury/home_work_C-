d03917226312eff383647f2093f15a48f468aa3d
Sylman <l.musilmanbekova12@gmail.com>
2023-05-07 05:33:26 -0700

    completed task047

---
+++ home_work_7/task047/Program.cs
@@ -0,0 +1,52 @@
+﻿/* using System.Threading.Tasks; */
+/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] FillDoubleArray(int m, int n)
+{
+    double[,] arr = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = Math.Round(rnd.Next(-100, 101) * 0.1, 1);
+        }
+    }
+    return arr;
+}
+
+void Print(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]} ");
+        System.Console.WriteLine();
+    }
+}
+
+/* double[,] FillMatrix(int rows, int cols)
+{
+    double[,] array = new double[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = Math.Round(rand.Next(-100, 101) * 0.1, 1);
+        }
+    }
+    return array;
+} */
+
+void Task047()
+{
+    Print(FillDoubleArray(3, 4));
+}
+
+Task047();
+
