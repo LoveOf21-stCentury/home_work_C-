8fa94d6aff1bc70c38541d522e343cfe4658547a
Sylman <l.musilmanbekova12@gmail.com>
2023-05-09 09:11:53 -0700

    doesn't completed task052

---
+++ home_work_7/task052/Program.cs
@@ -0,0 +1,44 @@
+﻿/*  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int[,] CreateTwoArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    
+    for (int i = 0; i < m(0); i++)
+        for (int j = 0; j < n(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    return array;
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        System.Console.WriteLine();
+    }
+}
+
+void FindMiddleOfArethMean(int[,] array)
+{
+    double res = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            res[i] += array[i][j];
+
+        res[i] /= n;
+    }
+    return res;
+}
+
+Print(CreateTwoArray(3, 4));
+FindMiddleOfArethMean(array);
