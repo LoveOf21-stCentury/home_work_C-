82648710a6c0346192e91bf3d0a786cd056015b5
Sylman <l.musilmanbekova12@gmail.com>
2023-05-22 09:04:15 -0700

    task054

--- home_work_8/task054/Program.cs
+++ home_work_8/task054/Program.cs
@@ -8,12 +8,57 @@
 9 5 3 2
 8 4 4 2 */
 
-void FillArray()
+void FillArray(int[,] array)
 {
-
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
 };
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void SortMarix(int[,] array, int row, int column)
+{
+    int temp = 0;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column - 1; j++)
+        {
+            if (array[i, j] > array[i, j + 1])
+            {
+                temp = array[i, j];
+                array[i, j] = array[i, j + 1];
+                array[i, j + 1] = temp;
+            }
+        }
+    }
+
+}
 
 void Task054()
 {
-
+    int column = 5;
+    int row = 4;
+    int[,] array = new int[row, column];
+    FillArray(array);
+    Print(array);
+    SortMarix(array, row, column);
+    Print(array);
 }
+Task054();
