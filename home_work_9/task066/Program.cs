21fde34e769d143ff3f451960d2625a807b6522d
Sylman <l.musilmanbekova12@gmail.com>
2023-05-28 03:44:05 -0700

    completed task066

--- home_work_9/task066/Program.cs
+++ home_work_9/task066/Program.cs
@@ -3,3 +3,19 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
 
+void Print(int m, int n)
+{
+    System.Console.Write($"{m} ");
+    if (m != n)
+    {
+        Print(m + 1, n);
+    }
+}
+
+void Task066()
+{
+    int m = 4;
+    int n = 8;
+    Print(m, n);
+}
+Task066();
