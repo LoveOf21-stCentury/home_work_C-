a428813366f5428362fb9bb65fa9f70ca18af8e0
Sylman <l.musilmanbekova12@gmail.com>
2023-05-28 03:32:41 -0700

    added hw_9 and complited task064

---
+++ home_work_9/task064/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int UserNumb(int numb)
+{
+    System.Console.WriteLine($"write element: ");
+    numb = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine();
+    return numb;
+}
+
+void Print(int numb)
+{
+    if (numb != 1)
+    {
+        Print(numb - 1);
+    }
+    System.Console.Write($"{numb} ");
+}
+
+void Task64()
+{
+    int numb = 0;
+    Print(UserNumb(numb));
+}
+Task64();
